6d4418163dd4a164b8ca3fd3f49aba863544c239
Caleb Einarr <earthctzn1@gmail.com>
2019-04-08 19:31:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,10 @@
 def my_collect(array)
-
+    new_a = []
     count = 0 
-     while i < array.length
+     while count < array.length
       yield(array[i])
       i = i + 1 
     end
-    return yield
-  else
-    i = 0
+  
   end
 end
