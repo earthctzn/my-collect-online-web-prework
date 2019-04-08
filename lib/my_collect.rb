2d9c60dffda1363d5f53e699ca2e389c522014a8
Caleb Einarr <earthctzn1@gmail.com>
2019-04-08 19:13:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,10 @@
-def my_collect(name)
+def my_collect(array)
   if block_given?
     i = 0 
-    
+     while i < array.length
+      yield(array[i])
+      i = i + 1 
+    end
+    return yeald 
 
 end
